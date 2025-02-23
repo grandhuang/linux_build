ec09fce0a6446c24f3c578a179756f21a3f69c0a
grandhuang <985919155@qq.com>
2025-02-23 14:31:47 +0800

    [编译][LWIP]修改LWIP移植编译源码列表;移植问题待解决;修改编译信息保存格式

--- lwip/package.mk
+++ lwip/package.mk
@@ -1,49 +1,117 @@
-LWIP_DIR = lwip
 
-LWIP_SOURCES = $(LWIP_DIR)/src/core/altcp_alloc.c   \
-			$(LWIP_DIR)/src/core/altcp.c         \
-			$(LWIP_DIR)/src/core/altcp_tcp.c     \
-			$(LWIP_DIR)/src/core/def.c           \
-			$(LWIP_DIR)/src/core/dns.c           \
-			$(LWIP_DIR)/src/core/inet_chksum.c   \
-			$(LWIP_DIR)/src/core/init.c          \
-			$(LWIP_DIR)/src/core/ip.c            \
-			$(LWIP_DIR)/src/core/mem.c           \
-			$(LWIP_DIR)/src/core/memp.c          \
-			$(LWIP_DIR)/src/core/netif.c         \
-			$(LWIP_DIR)/src/core/pbuf.c          \
-			$(LWIP_DIR)/src/core/raw.c           \
-			$(LWIP_DIR)/src/core/stats.c         \
-			$(LWIP_DIR)/src/core/sys.c           \
-			$(LWIP_DIR)/src/core/tcp.c           \
-			$(LWIP_DIR)/src/core/tcp_in.c        \
-			$(LWIP_DIR)/src/core/tcp_out.c       \
-			$(LWIP_DIR)/src/core/timeouts.c      \
-			$(LWIP_DIR)/src/core/ipv4/udp.c           \
-			$(LWIP_DIR)/src/core/ipv4/acd.c           \
-			$(LWIP_DIR)/src/core/ipv4/autoip.c        \
-			$(LWIP_DIR)/src/core/ipv4/dhcp.c          \
-			$(LWIP_DIR)/src/core/ipv4/etharp.c        \
-			$(LWIP_DIR)/src/core/ipv4/icmp.c          \
-			$(LWIP_DIR)/src/core/ipv4/igmp.c          \
-			$(LWIP_DIR)/src/core/ipv4/ip4_addr.c      \
-			$(LWIP_DIR)/src/core/ipv4/ip4.c           \
-			$(LWIP_DIR)/src/core/ipv4/ip4_frag.c      \
-			$(LWIP_DIR)/src/core/ipv6/dhcp6.c         \
-			$(LWIP_DIR)/src/core/ipv6/ethip6.c        \
-			$(LWIP_DIR)/src/core/ipv6/icmp6.c         \
-			$(LWIP_DIR)/src/core/ipv6/inet6.c         \
-			$(LWIP_DIR)/src/core/ipv6/ip6_addr.c      \
-			$(LWIP_DIR)/src/core/ipv6/ip6.c           \
-			$(LWIP_DIR)/src/core/ipv6/ip6_frag.c      \
-			$(LWIP_DIR)/src/core/ipv6/mld6.c          \
-			$(LWIP_DIR)/src/core/ipv6/nd6.c           \
+LWIPDIR = lwip/src
+# COREFILES, CORE4FILES: The minimum set of files needed for lwIP.
+COREFILES=$(LWIPDIR)/core/init.c \
+	$(LWIPDIR)/core/def.c \
+	$(LWIPDIR)/core/dns.c \
+	$(LWIPDIR)/core/inet_chksum.c \
+	$(LWIPDIR)/core/ip.c \
+	$(LWIPDIR)/core/mem.c \
+	$(LWIPDIR)/core/memp.c \
+	$(LWIPDIR)/core/netif.c \
+	$(LWIPDIR)/core/pbuf.c \
+	$(LWIPDIR)/core/raw.c \
+	$(LWIPDIR)/core/stats.c \
+	$(LWIPDIR)/core/sys.c \
+	$(LWIPDIR)/core/altcp.c \
+	$(LWIPDIR)/core/altcp_alloc.c \
+	$(LWIPDIR)/core/altcp_tcp.c \
+	$(LWIPDIR)/core/tcp.c \
+	$(LWIPDIR)/core/tcp_in.c \
+	$(LWIPDIR)/core/tcp_out.c \
+	$(LWIPDIR)/core/timeouts.c \
+	$(LWIPDIR)/core/udp.c
 
+CORE4FILES=$(LWIPDIR)/core/ipv4/acd.c \
+	$(LWIPDIR)/core/ipv4/autoip.c \
+	$(LWIPDIR)/core/ipv4/dhcp.c \
+	$(LWIPDIR)/core/ipv4/etharp.c \
+	$(LWIPDIR)/core/ipv4/icmp.c \
+	$(LWIPDIR)/core/ipv4/igmp.c \
+	$(LWIPDIR)/core/ipv4/ip4_frag.c \
+	$(LWIPDIR)/core/ipv4/ip4.c \
+	$(LWIPDIR)/core/ipv4/ip4_addr.c
 
+CORE6FILES=$(LWIPDIR)/core/ipv6/dhcp6.c \
+	$(LWIPDIR)/core/ipv6/ethip6.c \
+	$(LWIPDIR)/core/ipv6/icmp6.c \
+	$(LWIPDIR)/core/ipv6/inet6.c \
+	$(LWIPDIR)/core/ipv6/ip6.c \
+	$(LWIPDIR)/core/ipv6/ip6_addr.c \
+	$(LWIPDIR)/core/ipv6/ip6_frag.c \
+	$(LWIPDIR)/core/ipv6/mld6.c \
+	$(LWIPDIR)/core/ipv6/nd6.c
 
-LWIP_CFLAGS = -I$(LWIP_DIR)/src/include	\
-				-I$(LWIP_DIR)/src/include/lwip	\
-				-I$(LWIP_DIR)/contrib	\
-				-I$(LWIP_DIR)/contrib/examples/example_app\
-				-I$(LWIP_DIR)/contrib/ports/unix/port/include
+# APIFILES: The files which implement the sequential and socket APIs.
+APIFILES=$(LWIPDIR)/api/api_lib.c \
+	$(LWIPDIR)/api/api_msg.c \
+	$(LWIPDIR)/api/err.c \
+	$(LWIPDIR)/api/if_api.c \
+	$(LWIPDIR)/api/netbuf.c \
+	$(LWIPDIR)/api/netdb.c \
+	$(LWIPDIR)/api/netifapi.c \
+	$(LWIPDIR)/api/sockets.c \
+	$(LWIPDIR)/api/tcpip.c
+
+# NETIFFILES: Files implementing various generic network interface functions
+NETIFFILES=$(LWIPDIR)/netif/ethernet.c \
+	$(LWIPDIR)/netif/bridgeif.c \
+	$(LWIPDIR)/netif/bridgeif_fdb.c \
+	$(LWIPDIR)/netif/slipif.c
+
+# SIXLOWPAN: 6LoWPAN
+SIXLOWPAN=$(LWIPDIR)/netif/lowpan6_common.c \
+        $(LWIPDIR)/netif/lowpan6.c \
+	$(LWIPDIR)/netif/lowpan6_ble.c \
+	$(LWIPDIR)/netif/zepif.c
+
+# PPPFILES: PPP
+PPPFILES=$(LWIPDIR)/netif/ppp/auth.c \
+	$(LWIPDIR)/netif/ppp/ccp.c \
+	$(LWIPDIR)/netif/ppp/chap-md5.c \
+	$(LWIPDIR)/netif/ppp/chap_ms.c \
+	$(LWIPDIR)/netif/ppp/chap-new.c \
+	$(LWIPDIR)/netif/ppp/demand.c \
+	$(LWIPDIR)/netif/ppp/eap.c \
+	$(LWIPDIR)/netif/ppp/ecp.c \
+	$(LWIPDIR)/netif/ppp/eui64.c \
+	$(LWIPDIR)/netif/ppp/fsm.c \
+	$(LWIPDIR)/netif/ppp/ipcp.c \
+	$(LWIPDIR)/netif/ppp/ipv6cp.c \
+	$(LWIPDIR)/netif/ppp/lcp.c \
+	$(LWIPDIR)/netif/ppp/magic.c \
+	$(LWIPDIR)/netif/ppp/mppe.c \
+	$(LWIPDIR)/netif/ppp/multilink.c \
+	$(LWIPDIR)/netif/ppp/ppp.c \
+	$(LWIPDIR)/netif/ppp/pppapi.c \
+	$(LWIPDIR)/netif/ppp/pppcrypt.c \
+	$(LWIPDIR)/netif/ppp/pppoe.c \
+	$(LWIPDIR)/netif/ppp/pppol2tp.c \
+	$(LWIPDIR)/netif/ppp/pppos.c \
+	$(LWIPDIR)/netif/ppp/upap.c \
+	$(LWIPDIR)/netif/ppp/utils.c \
+	$(LWIPDIR)/netif/ppp/vj.c \
+	$(LWIPDIR)/netif/ppp/polarssl/arc4.c \
+	$(LWIPDIR)/netif/ppp/polarssl/des.c \
+	$(LWIPDIR)/netif/ppp/polarssl/md4.c \
+	$(LWIPDIR)/netif/ppp/polarssl/md5.c \
+	$(LWIPDIR)/netif/ppp/polarssl/sha1.c
+
+# All LWIP files without apps
+LWIP_SOURCES=$(COREFILES) \
+	$(CORE4FILES) \
+	$(CORE6FILES) \
+	$(APIFILES) \
+	$(NETIFFILES) \
+	$(PPPFILES) \
+	$(SIXLOWPAN)
+
+LWIP_CFLAGS = -I$(LWIPDIR)/../src/include	\
+				-I$(LWIPDIR)/../contrib	\
+				-I$(LWIPDIR)/../contrib/examples/example_app	\
+				-I$(LWIPDIR)/../contrib/ports	\
+				-I$(LWIPDIR)/../contrib/ports/unix/port/include	\
+				-lpthread -ldl -lm -lrt -lutil -lncursesw \
+				-DLWIP_DEFINITIONS	\
+				-DLWIP_DEBUG=1
 
